03436aa515564812b40122f51ccaa770ab1a983d
David Wallin <parmesan@datawrangler.ninja>
2018-12-09 12:00:29 +0100

    remove she-bang (we'll use setuptools)

--- parmesan/parmesan.py
+++ parmesan/parmesan.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python
 import errno
 import os
-# from pathlib import Path
 
 import numpy as np
 import pandas as pd
@@ -53,34 +51,6 @@ def convert_qvd_to_parquet(qvd_file, pq_file):
 def convert_qvd_tree(qvd_prefix, pq_prefix, recursive=False):
     pass
 
-
-if __name__ == '__main__':
-    import sys
-    from IPython import embed
-    if len(sys.argv) > 1:
-        q = QvdFile()
-        q.Load(sys.argv[1])
-        th = q.GetTableHeader()
-        idx_mtx = np.array(th.Indices, dtype=np.int64).reshape((th.NoOfRecords, -1))
-        df = pd.DataFrame(idx_mtx)
-
-        # insert missing columns; columns are missing if they only contain a single symbol
-        for i, col in enumerate(th.Fields):
-            if col.BitWidth == 0:
-                df.insert(i, f'{i}a', 0, True)
-        df.columns = range(len(th.Fields))
-
-        # buildup a map that renames indices to their corresponding symbol
-        idx_mapping = {i: pd.Series(transform_symbol_type(*get_symbols(fld))).to_dict()
-                       for (i, fld) in enumerate(th.Fields)}
-        df2 = pd.concat([df[i].map(idx_mapping[i]) for i in df], axis=1)
-
-        colnames = [x.FieldName for x in th.Fields]
-        df2.columns = colnames
-
-        #df = read_qvd(sys.argv[1])
-
-    embed()
 
 ## Local Variables: ***
 ## mode:python ***
